1db96a5d90e8b1a11d95838a53475a90552536c7
loheagn <linan_123@buaa.edu.cn>
2019-12-22 12:19:17 +0800

    最终成果

--- src/main/java/com/wwt/gateway/GatewayApplication.java
+++ src/main/java/com/wwt/gateway/GatewayApplication.java
@@ -13,13 +13,23 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 
-	@Bean
-	public RouteLocator userRouteLocator(RouteLocatorBuilder builder) {
-		return builder.routes().route(r -> r.path("/v1/users/**").filters(f->f.stripPrefix(1)).uri("lb://USER-SERVICE").filter(new AuthenticateFilter()).id("user")).build();
-	}
+    @Bean
+    public RouteLocator userRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes().route(r -> r.path("/v1/users/**").filters(f -> f.stripPrefix(1)).uri("lb://USER-SERVICE").filter(new AuthenticateFilter()).id("user")).build();
+    }
+
+    @Bean
+    public RouteLocator paperRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes().route(r -> r.path("/v1/papers/**").filters(f -> f.stripPrefix(1)).uri("lb://PAPER-SERVICE").filter(new AuthenticateFilter()).id("paper")).build();
+    }
+
+    @Bean
+    public RouteLocator expertRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes().route(r -> r.path("/v1/experts/**").filters(f -> f.stripPrefix(1)).uri("lb://EXPERT-SERVICE").filter(new AuthenticateFilter()).id("expert")).build();
+    }
 
 }
